bde45b2435cd4d5330e1418a735a12d6e4f8ccff
nadeemkhan6993 <nadeemkhan6993@gmail.com>
2020-08-30 06:40:17 +0530

    Updates

---
+++ precache-manifest.e77f7ceeb00578856298f4e922541a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c3ce75e94cae4a2d87d32734dcee34",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "c6094e4cd3e766d18d61",
+    "url": "/todo-react/static/css/main.f964867c.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/todo-react/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6094e4cd3e766d18d61",
+    "url": "/todo-react/static/js/main.17ff3b3f.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
